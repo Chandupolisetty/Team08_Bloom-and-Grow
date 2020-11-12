3dd2288b6c75d7acf52e55c0ecfe45b06dde5cdc
Chandupolisetty <chandrakanthpolisetty@gmail.com>
2020-11-11 23:29:21 -0600

    Added some of the views for the applications
    Added Some of the Views for the application

---
+++ Team08_Bloom_&_Grow/Team08_Bloom_&_Grow/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Team08_Bloom_&_Grow
+//
+//  Created by student on 11/11/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
