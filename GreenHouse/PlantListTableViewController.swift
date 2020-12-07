11e93481c7bda7edeb80507f578e1e6181e03e11
bhumahareesh <hareeshbhuma@gmail.com>
2020-12-07 00:59:34 -0600

    Added PlantList, Plants and Delegate
    Added PlantList, Plants and Delegate controller swift files

---
+++ GreenHouse/PlantListTableViewController.swift
@@ -0,0 +1,101 @@
+
+
+import UIKit
+
+class PlantListTableViewController: UITableViewController {
+    
+    var plant: Plant!
+    
+    // currentCell records the index of the selected cell to be used in other pages without a tableview
+    struct Global {
+        static var currentCell: Int!
+    }
+    
+    var plants: [String] = ["Spikey the Cactus", "Lily the Snake Plant", "Yoshi the Yucca"]
+    
+    let nameLBLTag = 10
+    let optimalRowHeight:CGFloat = 100
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "My Plants"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: .plain, target: self, action: #selector(settings(sender:)))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addPlant(sender:)))
+        NotificationCenter.default.addObserver(self, selector: #selector(plantAdded(notification:)), name: NSNotification.Name(rawValue: "Added Plant"), object: nil)
+    }
+
+    // Refresh table view after adding a plant
+    @objc func plantAdded(notification:Notification) {
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Plants.shared.numPlants()
+    }
+    
+    // Height of each table cell
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return optimalRowHeight
+    }
+
+    // Contents of table cells
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "plantCell")!
+        if let plant = Plants.shared[indexPath.row] {
+            let nameLBL = cell.viewWithTag(nameLBLTag) as! UILabel
+            
+            if plant.name == "" { // If the plant doesn't have a name, only display species
+                nameLBL.text = plant.species
+            }
+            else if plant.species == "" {  // If the plant doesn't have a specified species, only display name
+                nameLBL.text = plant.name
+            }
+            else { // If the plant has both, combine both with "the" in between
+                nameLBL.text = plant.name + " the " + plant.species
+            }
+        }
+        return cell
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            Plants.shared.delete(index: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let plantInfoViewCont = (storyboard?.instantiateViewController(identifier: Constants.plantInfoViewController) as? PlantInfoViewController)!
+        plantInfoViewCont.plant = Plants.shared[indexPath.row]
+        navigationController?.pushViewController(plantInfoViewCont, animated: true)
+        
+        // Set currentCell to the selected row so that it can be used in PlantInfoViewController
+        Global.currentCell = indexPath.row
+    }
+
+    @objc
+    func settings(sender: Any) {
+        let settingsViewCont = (storyboard?.instantiateViewController(identifier: Constants.settingsViewController) as? SettingsViewController)!
+
+            //self.present(settingsViewCont, animated: true)
+        navigationController?.pushViewController(settingsViewCont, animated: true)
+    }
+
+    @objc
+    func addPlant(sender: Any) {
+        let addPlantViewCont = (storyboard?.instantiateViewController(identifier: Constants.addPlantViewController) as? AddPlantViewController)!
+
+        self.present(addPlantViewCont, animated: true, completion: nil)
+    }
+    
+}
---
+++ GreenHouse/Plants.swift
@@ -0,0 +1,75 @@
+
+
+import Foundation
+
+/// The fundamental unit in the model, a Plant
+struct Plant {
+    var name: String
+    var species: String
+    var waterFrequency: Int
+    var waterTimeFrame: String
+    var sunlightDuration: Int
+    var sunlightTimeFrame: String
+}
+
+class Plants {
+    
+    private static var _shared:Plants! // Only visible in this struct
+    
+    static var shared: Plants { // To access this anywhere, in the app just write Plants.shared
+        if _shared == nil {
+            _shared = Plants()
+        }
+        return _shared
+    }
+    
+    private var plants:[Plant] = [
+        Plant(name: "Spikey", species: "Cactus", waterFrequency: 1, waterTimeFrame: "week", sunlightDuration: 8, sunlightTimeFrame: "day"),
+        Plant(name: "Lily", species: "Snake Plant", waterFrequency: 2, waterTimeFrame: "day", sunlightDuration: 2, sunlightTimeFrame: "day"),
+        Plant(name: "Yoshi", species: "Yucca", waterFrequency: 3, waterTimeFrame: "week", sunlightDuration: 2, sunlightTimeFrame: "day"),
+        Plant(name: "", species: "Tomato Plant", waterFrequency: 2, waterTimeFrame: "day", sunlightDuration: 6, sunlightTimeFrame: "day"),
+        Plant(name: "Lucus", species: "", waterFrequency: 1, waterTimeFrame: "day", sunlightDuration: 1, sunlightTimeFrame: "day")
+    ]
+    
+    private init(){
+    
+    }
+    
+    // Returns plant at a given index, nil if non-existant
+    /// Example usage:  Plants.shared.getPlant(at:5)
+    /// Parameter index: plant to return
+    func getPlant(at index:Int)->Plant? {
+        if index >= 0 && index < plants.count {
+            return plants[index]
+        }
+        else {
+            return nil
+        }
+    }
+    
+    /// Subscript Plants, so usage would be Plants.shared[5]
+    subscript(index: Int) -> Plant? {
+        return index >= 0 && index < plants.count ? plants[index] : nil
+    }
+    
+    // Returns the # of plants
+    func numPlants() -> Int {
+        return plants.count
+    }
+    
+    // Adds a plant to the collection
+    /// Example usage: Plants.shared.add(plant:Plant(name: "", species: "Tomato Plant"), waterFrequency: [1: "day"], sunlightFrequency: [6: "day"])
+    /// Parameter plant: plant to add
+    func add(plant: Plant){
+        plants.append(plant)
+    }
+    
+    func delete(index: Int){
+        plants.remove(at: index)
+    }
+    
+    func edit(index: Int, plant: Plant) {
+        plants[index] = plant
+    }
+    
+}
